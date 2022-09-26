e08bfec5936a1995315432944926c5189ceff7a0
muhammadaqib1994 <108095341+muhammadaqib1994@users.noreply.github.com>
2022-09-26 03:10:17 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
-variable "APP_NAME" {
+variable "CLUSTER_NAME" {
   type        = string
   description = "Name for the eks cluster."
   default     = "test-cluster"
 }
 
 variable "CLUSTER_VERSION" {
-  default     = "latest_version"
+  default     = "1.20"
   type        = string
   description = "Kubernetes version in EKS."
 }
@@ -33,54 +33,12 @@ variable "WORKERS_SUBNETS" {
   default     = ["subnet-073b537ecd7c2efa7", "subnet-0f2143f26f35ea0b6"]
 }
 
-# variable "bucket" {
-#   type =  string
-#   default = "sohail-terraform-state"
-# }
-# variable "bucket_key" {
-#   type = string
-#   default = "sohail/gitlab-runner/terraform.tfstate"
-# }
 
 variable "API_PRIVATE_ACCESS" {
   type    = bool
   default = false
 }
 
-# variable "ENGINE" {
-#   type    = string
-#   default = "mysql"
-# }
-
-# variable "ENGINE_VERSION" {
-#   type    = string
-#   default = "5.7"
-# }
-
-# variable "DB_NAME" {
-#   type    = string
-#   default = "test"
-# }
-
-# variable "DB_USERNAME" {
-#   type    = string
-#   default = "testuser"
-# }
-
-# variable "DB_PASSWORD" {
-#   type    = string
-#   default = "testuserpassword"
-# }
-
-# variable "INSTANCE_CLASS" {
-#   type    = string
-#   default = "db.t3.micro"
-# }
-
-# variable "DB_PARAMETER_GROUP_NAME" {
-#   type    = string
-#   default = "default.mysql5.7"
-# }
 
 variable "AWS_REGION" {
   type    = string
